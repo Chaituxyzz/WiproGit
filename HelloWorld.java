8f1f0f7c7c506cf1cbc04e7a5175627cdca83bca
Chaituxyzz <buridichaitanya@gmail.com>
2025-01-27 13:29:49 +0530

    my first commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,8 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
-    }
+	int a = 10;
+	int b=100;
+	int c=a+b;
+	System.out.println(c+" "+a);
 }
